997cca18c8f2a8030d045f0f469474201b415d27
yeonsoo89 <dustn9689@gmail.com>
2025-11-14 11:37:29 +0900

    feature : 관리자 페이지 상점 관리 목록 추가

---
+++ src/main/java/pinup/backend/store/command/controller/StoreAdminViewController.java
@@ -0,0 +1,22 @@
+package pinup.backend.store.command.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pinup.backend.store.query.service.StoreQueryService;
+
+@Controller
+@RequiredArgsConstructor
+public class StoreAdminViewController {
+
+    private final StoreQueryService storeQueryService;
+
+    // 아이템 목록 페이지
+    @GetMapping("/admin/store")
+    public String storeList(Model model) {
+        model.addAttribute("itemList", storeQueryService.getActiveItems());
+        return "admin/store";
+    }
+}
