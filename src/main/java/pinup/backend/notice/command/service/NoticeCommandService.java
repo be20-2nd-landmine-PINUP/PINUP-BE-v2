7aea98fd204f46a2765901d1429eb164804303b8
405supply <405supplychoi@gmail.com>
2025-11-26 10:30:31 +0900

    refactor : 인증 실패 시 오류 핸들링 추가

--- src/main/java/pinup/backend/notice/command/service/NoticeCommandService.java
+++ src/main/java/pinup/backend/notice/command/service/NoticeCommandService.java
@@ -2,10 +2,12 @@ package pinup.backend.notice.command.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 import pinup.backend.auth.command.service.CustomAdminDetailsService;
 import pinup.backend.member.command.domain.Admin;
 import pinup.backend.member.command.repository.AdminRepository;
@@ -23,7 +25,9 @@ public class NoticeCommandService {
     private final AdminRepository adminRepository;
 
     public Long postNotice(NoticePostRequest request, User user) {
-        Admin admin = adminRepository.findByName(user.getUsername()).orElseThrow(IllegalStateException::new);
+        Admin admin = adminRepository.findByName(user.getUsername()).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin not found")
+        );
 
         return noticeRepository.save(Notice.builder()
                 .noticeContent(request.getNoticeContent())
@@ -33,15 +37,22 @@ public class NoticeCommandService {
     }
 
     public void patchNotice(NoticePatchRequest request, User user) {
-        Admin admin = adminRepository.findByName(user.getUsername()).orElseThrow(IllegalStateException::new);
-        Notice notice = noticeRepository.findById(request.getNoticeId()).orElseThrow(IllegalArgumentException::new);
+        Admin admin = adminRepository.findByName(user.getUsername()).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin not found")
+        );
+        Notice notice = noticeRepository.findById(request.getNoticeId()).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Notice not found")
+        );
 
         notice.patchNotice(request);
         notice.patchAuthor(admin);
     }
 
     public void deleteNotice(Long id) {
-        Notice notice = noticeRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        Notice notice = noticeRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Notice not found")
+        );
+
         noticeRepository.delete(notice);
     }
 }
