04296bf9c7471bbc6b20a7bfb2efadd4c48421d9
longlive13 <papiairplane@gmail.com>
2025-11-26 11:16:38 +0900

    feature : 호출 결과에 따른 수정(response 내용 수정)

--- src/main/java/pinup/backend/recommendation/query/RecommendationResponseDTO.java
+++ src/main/java/pinup/backend/recommendation/query/RecommendationResponseDTO.java
@@ -10,5 +10,4 @@ public class RecommendationResponseDTO {
     private String region;       // 추천된 최종 지역명
     private String title;        // 한 줄 제목
     private String description;  // 추천 이유/설명
-    private Long setRegionId; //없애도 되긴하는데..
 }
