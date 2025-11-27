a6459989dd7c3c2c4203d45a915bba71eb7fd742
longlive13 <papiairplane@gmail.com>
2025-11-27 10:40:09 +0900

    feature : 백업파일 copy

--- src/main/java/pinup/backend/recommendation/domain/RecommendRepository.java
+++ src/main/java/pinup/backend/recommendation/domain/RecommendRepository.java
@@ -1,4 +1,11 @@
 package pinup.backend.recommendation.domain;
 
-public class RecommendRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RecommendRepository extends JpaRepository<Recommend, Long> {
+    // userId 기준으로 "가장 최근" 추천 1개
+    Optional<Recommend> findTopByUserUserIdOrderByRecommendAtDesc(Long userId);
+
 }
