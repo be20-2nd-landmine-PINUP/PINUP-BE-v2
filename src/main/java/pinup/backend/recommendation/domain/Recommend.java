3e47c475fb5be435567562bae11dc21c19a0bd84
longlive13 <papiairplane@gmail.com>
2025-11-27 10:39:30 +0900

    feature : 백업파일 copy

--- src/main/java/pinup/backend/recommendation/domain/Recommend.java
+++ src/main/java/pinup/backend/recommendation/domain/Recommend.java
@@ -1,4 +1,46 @@
 package pinup.backend.recommendation.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pinup.backend.member.command.domain.Users;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "recommend")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Recommend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recommend_id")
+    private Long recommendId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @Column(name = "recommend_spot", length = 500, nullable = false)
+    private String recommendSpot;
+
+    // ✅ 추천된 지역(또는 앵커 스팟)의 ID
+    //@Column(name = "region_id")
+    //private Long regionId;
+
+    @Column(name = "reason", columnDefinition = "TEXT", nullable = false)
+    private String reason;
+
+    @Column(name = "recommend_at", nullable = false)
+    private LocalDateTime recommendAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.recommendAt = LocalDateTime.now();
+    }
 }
